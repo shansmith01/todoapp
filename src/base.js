696c9b90d0855357c92560903f0432f9c35427bf
Shannon Smith <shannon@powerboard.co.nz>
2019-03-29 19:03:11 +1300

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base"
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyChPh0fp089gllfzUIBmEuSXyWLP8W0cRM",
+  apiKey: "xxxxxx",
   authDomain: "todo-2926a.firebaseapp.com",
   databaseURL: "https://todo-2926a.firebaseio.com",
   projectId: "todo-2926a",
