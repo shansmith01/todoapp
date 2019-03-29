130b1ea55d665ced90b897f862d1213d1a499b43
Shannon Smith <shannon@powerboard.co.nz>
2019-03-29 18:58:09 +1300

    Update base.js

--- src/base.js
+++ src/base.js
@@ -2,7 +2,12 @@ import Rebase from "re-base"
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-  XXXX,
+  apiKey: "AIzaSyChPh0fp089gllfzUIBmEuSXyWLP8W0cRM",
+  authDomain: "todo-2926a.firebaseapp.com",
+  databaseURL: "https://todo-2926a.firebaseio.com",
+  projectId: "todo-2926a",
+  storageBucket: "todo-2926a.appspot.com",
+  messagingSenderId: "792169899935",
 })
 
 const base = Rebase.createClass(firebaseApp.database())
